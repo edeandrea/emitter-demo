dbed398070aaa315b70333c2b8640a85cd422ff0
Eric Deandrea <eric.deandrea@gmail.com>
2025-01-02 14:40:30 -0500

    Changing to MutinyEmitter

--- src/main/java/com/example/TaskResource.java
+++ src/main/java/com/example/TaskResource.java
@@ -8,29 +8,28 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 import io.quarkus.logging.Log;
 
-import io.smallrye.common.annotation.RunOnVirtualThread;
 import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.reactive.messaging.MutinyEmitter;
 
 @Path("/tasks")
 public class TaskResource {
 	private final Multi<Task> channel;
-	private final Emitter<Task> emitter;
+	private final MutinyEmitter<Task> emitter;
 
-	public TaskResource(@Channel("channel") Multi<Task> channel, @Channel("channel") Emitter<Task> emitter) {
+	public TaskResource(@Channel("channel") Multi<Task> channel, @Channel("channel") MutinyEmitter<Task> emitter) {
 		this.channel = channel;
 		this.emitter = emitter;
 	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	@RunOnVirtualThread
-	public void addTask(Task task) {
+	public Uni<Void> addTask(Task task) {
 		Log.infof("Adding new task: %s", task);
-		this.emitter.send(task);
+		return this.emitter.send(task);
 	}
 
 	@GET
